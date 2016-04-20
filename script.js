5f06f662359500c971dafdfdd6c0ff7d67ec5b3b
John <captainfailsauce@gmail.com>
2016-04-20 21:00:16 +0100

    Added files via upload

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-/**
- * Created by CaptainFailsauce on 14/04/2016.
+/*Created by CaptainFailsauce on 14/04/2016.
  */
-$(document).ready(function(){
-    for (x = 0; x < 8; x++) {
-        for (y = 0; y < 8; y++) {
-            $('.container').append('<div class="square"></div>')
+var gridSize = 8;
+var squareSize = Math.floor(960 / gridSize) - 2;
+$(document).ready(function () {
+    "use strict";
+    var drawGrid = function () {
+        var x = 0, y = 0;
+        for (x; x < gridSize; x++) {
+            for (y = 0; y < gridSize; y++) {
+                $('.container').append('<div class="square"></div>');
+            }
+            $('.square:last-child').addClass('clearLine');
         }
-        $('.container').append('<br>')
+        $('.square').width(squareSize).height(squareSize);
     }
-    $('.square').on("animationend", function(){
+    drawGrid();
+    $('.square').on("animationend", function () {
         $(this).removeClass("animated");
         $(this).css("background", "#fff")
     });
 
-    $('.square').on("mouseenter", function(){
+    $('.square').on("mouseenter", function () {
         $(this).removeClass("animated");
         $(this).stop(true);
         $(this).css("background", "#000")
     });
 
-    $('.square').on("mouseleave", function(){
+    $('.square').on("mouseleave", function () {
         $(this).addClass("animated");
-    })
+    });
 });
